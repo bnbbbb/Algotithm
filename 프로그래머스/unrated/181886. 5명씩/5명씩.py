0dea6ee6c849404a8c08d60db53bc2aa2514991e
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-06-20 22:21:45 +0900

    [unrated] Title: 5명씩, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.py
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.py
@@ -1,11 +1,2 @@
-import math
-
 def solution(names):
-    answer = []
-    result = []
-    length = math.ceil(len(names) / 5)
-    for i in range(length):
-        answer.append(names[5*i:5*(i+1)])
-    for i in answer:
-        result.append(i[0])
-    return result
+    return names[::5]
