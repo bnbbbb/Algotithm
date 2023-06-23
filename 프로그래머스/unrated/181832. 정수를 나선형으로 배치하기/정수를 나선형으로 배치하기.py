d11178b2728ef6671b1a5bc03d2acb6fa3c06b1c
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-06-23 20:30:23 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -0,0 +1,23 @@
+def solution(n):
+    arr = [[0] * n for _ in range(n)]
+    num = 1
+    top, bottom, left, right = 0, n - 1, 0, n - 1
+    while num <= n * n:
+        for i in range(left, right + 1):
+            arr[top][i] = num
+            num += 1
+        top += 1
+        for i in range(top, bottom + 1):
+            arr[i][right] = num
+            num += 1
+        right -= 1
+        for i in range(right, left - 1, -1):
+            arr[bottom][i] = num
+            num += 1
+        bottom -= 1
+        for i in range(bottom, top - 1, -1):
+            arr[i][left] = num
+            num += 1
+        left += 1
+        
+    return arr
