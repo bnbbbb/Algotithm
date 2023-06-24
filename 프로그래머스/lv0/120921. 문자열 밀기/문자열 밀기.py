eff16378427114844a5c98374eb1c28ca9758584
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-06-25 03:53:53 +0900

    [level 0] Title: 문자열 밀기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
@@ -1,12 +1,3 @@
-def solution(m, n):
-    answer = 0
-    le = len(m)
-    for i in range(le):
-        if m != n:
-            m = m[le-1:]+m[:le-1] 
-            answer += 1
-            if answer == len(m):
-                return -1
-        else:
-            return answer
-    return answer
+def solution(A,B):
+    BB = B*2
+    return BB.find(A)
