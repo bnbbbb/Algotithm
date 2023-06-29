53686f9ae374c98f4dec141869d15c06985a8c21
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-06-29 17:34:38 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,3 +1,5 @@
-import math
 def solution(price):
-    return math.floor(price * 0.8 if price >= 500000 else (price * 0.9 if price >= 300000 else (0.95 * price if price >= 100000 else price)))
+    discount_rates = {500000: 0.8, 300000: 0.9, 100000: 0.95, 0: 1}
+    for discount_price, discount_rate in discount_rates.items():
+        if price >= discount_price:
+            return int(price * discount_rate)
