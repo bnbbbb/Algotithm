9fceea2afd988e8676a091200447113d824a041e
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-07-02 17:59:56 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,15 +1,7 @@
-import string
-
-tmp = string.digits+string.ascii_lowercase
-
-def convert(num) :
-    q, r = divmod(num, 3)
-    if q == 0 :
-        return tmp[r] 
-    else :
-        return convert(q) + tmp[r]
 def solution(n):
     answer = ''
-    answer += convert(n)
-    answer = answer[::-1]
-    return int(answer,3)
+    while n >= 1:
+        answer += (str(n % 3))
+        n = n//3
+    a = int(answer,3)
+    return a
