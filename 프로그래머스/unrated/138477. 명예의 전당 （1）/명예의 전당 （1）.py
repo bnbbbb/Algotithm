c9f3086fae819b3b8b72ff4835ba26bd32e37cb5
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-07-04 23:27:33 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -2,14 +2,11 @@ def solution(k, score):
     answer = []
     b = []
     for i in score:
-        if len(answer) <= k:
-            answer.append(i)
-            answer.sort()
-        elif len(answer) > k and min(answer) > i :
-            continue
-        if len(answer) > k:
-            del answer[0]
-        b.append(min(answer))
+        b.append(i)
+        b.sort()
+        if len(b) > k:
+            del b[0]
+        answer.append(min(b))
             
             
-    return b
+    return answer
