125b78cf20026b58b3fe2cff4c86edb898ffcc16
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-07-05 01:05:39 +0900

    [level 1] Title: 모의고사, Time: 2.68 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,19 +1,20 @@
 def solution(answers):
-    rank = []
+    answer = []
     result = [0,0,0]
     a = [1, 2, 3, 4, 5]
     b = [2, 1, 2, 3, 2, 4, 2, 5]
     c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
-    for i, answer in enumerate(answers):
-        if answer == a[i%5]:
+    for i in range(len(answers)):
+        if answers[i] == a[i%5]:
             result[0] += 1
-        if answer == b[i%8]:
-            result[1] += 1
-        if answer == c[i%10]:
+        if answers[i] == b[i%8]:
+            result[1] += 1 
+        if answers[i] == c[i%10]:
             result[2] += 1
     
     for i in range(len(result)):
         if max(result) == result[i]:
-            rank.append(i+1)
-    return rank
+            answer.append(i+1)
+    print(answer)
+    return answer
