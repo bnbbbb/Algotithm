b10768cada8cc4e8b1e9ff0e5b85ba66c6887fb7
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-07-16 03:03:21 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -0,0 +1,9 @@
+def solution(n, words):
+    stack = []
+    for i in range(len(words)):
+        if words[i] in stack:
+            return [(i % n) +1, (i // n) + 1]
+        if i > 0 and words[i][0] != words[i-1][-1]:
+            return [(i % n) +1, (i // n) + 1]
+        stack.append(words[i])
+    return [0,0]
