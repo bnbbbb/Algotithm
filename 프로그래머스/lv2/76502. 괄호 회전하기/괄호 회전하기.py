125a7590cbd18651cede77aa8be6fffc4584dc11
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-07-24 00:27:44 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -0,0 +1,30 @@
+def solution(s):
+    answer = 0
+    n = 0
+    a = ''
+    stack = []
+    while n != len(s):
+        for i in s:
+            if i in '([{':
+                stack.append(i)
+            else:
+                if not stack:
+                    break
+                cur = stack.pop()
+                if i == ')' and cur != '(':
+                    break
+                
+                elif i == '}' and cur != '{':
+                    break
+                
+                elif i == '[' and cur != '[':
+                    break
+        else:
+            if not stack:
+                answer+=1
+                
+        n += 1
+        a = s[0]
+        s = s[1:]
+        s += a
+    return answer
