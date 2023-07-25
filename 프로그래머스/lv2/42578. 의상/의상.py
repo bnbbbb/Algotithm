021e421d18ddbf32d016ffea40f445f9eeddcb2f
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-07-26 00:52:31 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -0,0 +1,13 @@
+def solution(clothes):
+    clothes_count = {}
+    for _, category in clothes:
+        if category in clothes_count:
+            clothes_count[category] += 1
+        else:
+            clothes_count[category] = 1
+    
+    answer = 1
+    for count in clothes_count.values():
+        answer *= (count + 1)  
+    
+    return answer - 1  
