5c709941fcd32cf5f45c48fff94ffeafba39d26a
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-07-30 22:32:52 +0900

    [level 2] Title: 피로도, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -0,0 +1,15 @@
+from itertools import permutations
+def solution(k, dungeons):
+    answer = []
+    result = 0
+    ans = list(permutations(dungeons, len(dungeons)))
+    a = k 
+    for i in ans:
+        a = k
+        result = 0
+        for j in i:
+            if a >= j[0]:
+                result+= 1
+                a -= j[1]
+        answer.append(result)
+    return max(answer)
