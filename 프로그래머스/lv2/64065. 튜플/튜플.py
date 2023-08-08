bc02aadff8acd7728068722d65165fbb1b128146
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-08-08 17:56:01 +0900

    [level 2] Title: 튜플, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.py
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -4,8 +4,10 @@ def solution(s):
     answer = []
     result =  ast.literal_eval(s.replace("{", "[").replace("}", "]"))
     result = sorted(result, key= lambda x: len(x))
+    
     for i in result:
         for j in i:
             if j not in answer:
-                answer.append(j)
+                answer.append(j)  
+                
     return answer
