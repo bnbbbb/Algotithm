9863e2547900688ed2d90a1bf2a2eb8f2f99b20c
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-08-10 17:26:17 +0900

    [level 2] Title: 더 맵게, Time: 1821.35 ms, Memory: 51.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,14 @@
+import heapq
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)  
+    while scoville[0] < K:
+        if len(scoville) < 2:
+            return -1
+        
+        
+        new_scoville = heapq.heappop(scoville) + (heapq.heappop(scoville) * 2)
+        heapq.heappush(scoville, new_scoville)
+        answer += 1
+        
+    return answer
