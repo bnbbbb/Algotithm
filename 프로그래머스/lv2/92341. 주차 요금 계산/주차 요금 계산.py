894f503189ebc761cd3481951497dea6ecc8a3f9
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-08-10 23:31:53 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.02 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -1,39 +1,32 @@
-import copy
 import math
 
-def time_to_min(time):
-    s_time = time.split(':')
-    second = int(s_time[0])*60 + int(s_time[1])
-    return second
-
 def solution(fees, records):
-    records = list(map(lambda x : x.split(), records))
-    arr = [0]*time_to_min('23:59')
-    times = {}
+    check = {}
+
     for record in records:
-        car_num = int(record[1])
-        if car_num not in times:
-            new_arr = copy.deepcopy(arr)
-            new_arr[time_to_min(record[0])] = 1
-            times[car_num] = new_arr
-        else : 
-            if record[2] == 'IN':
-                times[car_num][time_to_min(record[0])] = 1
-            else:
-                times[car_num][time_to_min(record[0])] = -1
+        time, number, status = record.split()
+        time = time.split(':')
+        time = int(time[0])*60 + int(time[1])
+        if number not in check:
+            check[number] = (0, time, status)
+        if status == 'IN':
+            check[number] = (check[number][0], time, status)
+        elif status == 'OUT':
+            total_time, in_time, _ = check[number]
+            total_time += time - in_time
+            check[number] = (total_time, time, status)
 
-    for arr in times.values():
-        for i in range(1, time_to_min('23:59')):
-            arr[i] += arr[i-1]
-    for car_num, arr in times.items():
-        times[car_num] = sum(arr)
+    result = {}
 
-    for car_num, time in times.items():
-        if time <= fees[0]:
-            times[car_num] = fees[1]
-        
+    for number in check.keys():
+        total_time, time, status = check[number]
+        if status == 'IN':
+            total_time += 1439 - time
+        fee = fees[1]
+        if total_time <= fees[0]:
+            result[number] = fee
         else:
-            times[car_num] = fees[1] + math.ceil((time-fees[0])/fees[2])*fees[-1]
-    answer = list(map(lambda x : x[1], sorted(times.items())))
+            fee = fee + math.ceil((total_time - fees[0]) / fees[2]) * fees[-1]
+            result[number] = fee
 
-    return answer
+    return list(map(lambda x : x[1], sorted(result.items())))
