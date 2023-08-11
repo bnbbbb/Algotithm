01ff0a3758329c7f962bc22517483b7ebadc659c
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-08-11 18:02:33 +0900

    [level 2] Title: 오픈채팅방, Time: 84.46 ms, Memory: 57.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
@@ -0,0 +1,18 @@
+def solution(record):
+    answer = []
+    check = {}
+    for i in range(len(record)):
+        record[i] = record[i].split()
+        if record[i][0] != 'Leave':
+            status, iden, name = record[i]
+            check[iden] = name
+    for i in range(len(record)):
+        if record[i][0] == 'Enter':
+            status, iden, name = record[i]
+            answer.append(f'{check[iden]}님이 들어왔습니다.')
+        elif record[i][0] == 'Leave':
+            status, iden = record[i]
+            answer.append(f'{check[iden]}님이 나갔습니다.')
+
+    
+    return answer
