abb93ed6f55ae7320d2b82815167e0795755ac81
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-08-13 20:51:06 +0900

    [level 2] Title: 할인 행사, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
@@ -7,6 +7,5 @@ def solution(want, number, discount):
     for i in range(len(discount)):
         result = Counter(discount[i:i+10])
         if all(result.get(j, 0) >= x for j, x in a.items()):
-            answer += 1
-            
+            answer += 1       
     return answer
