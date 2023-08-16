d53d2d570f76c8b3e15364142e893c5546b17f81
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-08-16 22:48:32 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 1.90 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -0,0 +1,12 @@
+import re
+def solution(files):
+    answer = []
+    for file in files:
+        matches = re.match(r'([a-zA-Z?.+\-\s]+)(\d+)', file)
+        if matches:
+            group1 = matches.group(1)
+            group2 = matches.group(2)
+            answer.append((group1, group2, file))
+    answer.sort(key= lambda x: (x[0].lower(), int(x[1])))
+    answer = [i[2] for i in answer]
+    return answer
