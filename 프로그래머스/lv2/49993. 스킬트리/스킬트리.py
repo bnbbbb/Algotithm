f59e718395c6963884a829fedc30932a6163c2c5
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-08-27 19:49:25 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
@@ -1,6 +1,5 @@
 def solution(skill, skill_trees):
     answer = 0
-    
     for ski in skill_trees:
         stack = ''
         for i in range(len(ski)):
@@ -8,4 +7,5 @@ def solution(skill, skill_trees):
                 stack += ski[i]
         if stack == skill or stack == skill[:len(stack)]:
             answer += 1
+            
     return answer
