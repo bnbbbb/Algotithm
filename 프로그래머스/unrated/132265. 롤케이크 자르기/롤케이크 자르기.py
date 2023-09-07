82012eafafd202869b1a52b0c48d69513344dcc1
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-09-07 14:30:00 +0900

    [unrated] Title: 롤케이크 자르기, Time: 607.32 ms, Memory: 51.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -0,0 +1,13 @@
+from collections import Counter
+def solution(topping):
+    answer = 0
+    bro = Counter(topping)
+    smbro = set()
+    for i in topping:
+        bro[i] -= 1
+        if bro[i] == 0:
+            del bro[i]
+        smbro.add(i)
+        if len(bro) == len(smbro):
+            answer += 1
+    return answer
