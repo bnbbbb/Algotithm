87e8523ce6a6a2a2eea3459fc93e6a8c3860ffd3
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-09-12 20:47:50 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,13 +1,10 @@
 def solution(number, k):
-    stack = []
-    
-    for num in number:
-        while k > 0 and stack and stack[-1] < num:
-            stack.pop()
+    stack = [number[0]]
+    for num in number[1:]:
+        while len(stack) > 0 and stack[-1] < num and k > 0:
             k -= 1
+            stack.pop()
         stack.append(num)
-    
-    if k > 0:
+    if k != 0:
         stack = stack[:-k]
-    
     return ''.join(stack)
