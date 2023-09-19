0ab5b3d1534d73752b56dcd8e6cd46e701ca2ad3
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-09-19 12:19:55 +0900

    [level 2] Title: 괄호 변환, Time: 0.15 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.py
@@ -0,0 +1,46 @@
+def solution(p):
+    if not p:
+        return ""
+    
+    u, v = split_p(p)
+    
+    if correct(u):
+        return u + solution(v)
+    
+    else:
+        answer = '(' + solution(v) + ')'
+        u = u[1:-1]
+        u = reverse(u)
+        answer += u
+        return answer
+
+def split_p(p):
+    left_count = 0
+    right_count = 0
+    for i in range(len(p)):
+        if p[i] == '(':
+            left_count += 1
+        else:
+            right_count += 1
+        if left_count == right_count:
+            return p[:i + 1], p[i + 1:]
+
+def correct(u):
+    stack = []
+    for char in u:
+        if char == '(':
+            stack.append(char)
+        else:
+            if not stack:
+                return False
+            stack.pop()
+    return True
+
+def reverse(u):
+    result =""
+    for char in u:
+        if char == '(':
+            result += ')'
+        else:
+            result += '('
+    return result
