c2596dc9eb78352c8ffd0f7157a6410a19791c16
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-09-27 23:41:22 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -2,38 +2,34 @@ def solution(places):
     def is_valid(x, y):
         return 0 <= x < 5 and 0 <= y < 5
 
-    def is_safe(x, y, place):
-        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-            nx, ny = x + dx, y + dy
-            if is_valid(nx, ny) and place[ny][nx] == 'P':
-                return False
-        return True
-
     def check_distance(place):
         for y in range(5):
             for x in range(5):
                 if place[y][x] == 'P':
-                    if not is_safe(x, y, place):
-                        return False
-                    for i in range(5):
-                        for j in range(5):
-                            if place[i][j] == 'P' and (x != j or y != i) and abs(x - j) + abs(y - i) <= 2:
-                                if x == j:
-                                    if place[min(y, i) + 1][x] != 'X':
-                                        return False
-                                elif y == i:
-                                    if place[y][min(x, j) + 1] != 'X':
-                                        return False
-                                else:
-                                    if place[y][j] != 'X' or place[i][x] != 'X':
-                                        return False
-        return True
+                    # 상하좌우 방향으로 거리두기 확인
+                    for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                        nx, ny = x + dx, y + dy
+                        if is_valid(nx, ny) and place[ny][nx] == 'P':
+                            return 0
+                    
+                    # 대각선 방향으로 거리두기 확인
+                    diagonals = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+                    for dx, dy in diagonals:
+                        nx, ny = x + dx, y + dy
+                        if is_valid(nx, ny) and place[ny][nx] == 'P':
+                            if place[y][nx] != 'X' or place[ny][x] != 'X':
+                                return 0
+                    
+                    # "P"와 "X" 사이에 "O"로 빈 자리가 있는 경우 처리
+                    for dx, dy in [(0, 2), (0, -2), (2, 0), (-2, 0)]:
+                        nx, ny = x + dx, y + dy
+                        if is_valid(nx, ny) and place[ny][nx] == 'P':
+                            if place[(y + ny) // 2][(x + nx) // 2] != 'X':
+                                return 0
+        return 1
 
     result = []
     for place in places:
-        if check_distance(place):
-            result.append(1)
-        else:
-            result.append(0)
+        result.append(check_distance(place))
     
     return result
