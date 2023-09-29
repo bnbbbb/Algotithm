334ff34a3e94a47b19b2b7de9fa1982850755f21
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-09-29 21:34:56 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 566.98 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
+++ 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
@@ -15,7 +15,6 @@ def solution(board):
         for j in range(1, cols):
             if board[i][j] == 1:
                 dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
-    
     for i in range(rows):
         for j in range(cols):
             answer = max(answer, dp[i][j])
