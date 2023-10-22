4cd1985529be07f8e4729e64e0dfc178ae086e3b
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-10-22 19:31:34 +0900

    [level 2] Title: 과제 진행하기, Time: 1.67 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -5,7 +5,6 @@ def solution(plans):
     def get_time(time):
         hour, minute = int(time[0:2]), int(time[3:5])
         return hour * 60 + minute
-
     plans = sorted(list(
         map(lambda x: [x[0], get_time(x[1]), int(x[2])], plans)), key=lambda x: x[1])
     answer, wait, now = [], deque([plans[0]]), plans[0][1]
