6ef8ab46c5a06e658299454f2d5c3e34962e1050
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-11-19 17:21:48 +0900

    [level 2] Title: 문자열 압축, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -0,0 +1,28 @@
+def solution(s):
+    result = ''
+    s_len = len(s)
+    idx  = 1
+    queue = []
+    if len(s) == 1:
+        return 1
+    while idx != s_len:
+        st = ''
+        answer = 0
+        stack = []
+        result = ''
+        for i in range(0, s_len, idx):
+            result = s[i+idx:i+idx+idx]
+            stack.append(s[i:i+idx])
+            if stack[0] !=  result:
+                if answer != 0:
+                    st += str(answer+1) + stack[0]
+                else:
+                    st += stack[0]
+                stack = []
+                answer = 0
+            else:
+                answer += 1
+        queue.append(st)
+        idx += 1
+    queue.sort(key=lambda x: len(x))
+    return len(queue[0])
