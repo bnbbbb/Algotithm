2e51a7090fd34ac0547dc17298a8944bec64aa7e
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-11-24 17:31:21 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,10 @@
+N, M = map(int, input().split())
+
+matrix_A = [list(map(int, input().split())) for _ in range(N)]
+
+matrix_B = [list(map(int, input().split())) for _ in range(N)]
+
+result_matrix = [[matrix_A[i][j] + matrix_B[i][j] for j in range(M)] for i in range(N)]
+
+for row in result_matrix:
+    print(*row)
