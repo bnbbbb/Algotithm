2040654ca3fd6d4eb1f62c1879f73a6a9fa05b76
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-12-03 18:38:30 +0900

    [Bronze II] Title: 블랙잭, Time: 100 ms, Memory: 42768 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -3,10 +3,9 @@ from itertools import combinations as co
 a, b = map(int, input().split())
 d = list(map(int, input().split()))
 li = list(co(d, 3))
-result = []
+result = 0
 for i in li:
-    if b-sum(i) >= 0:
-        result.append([sum(i), b-sum(i)])
-
-x = min(result, key=lambda x: x[1])
-print(x[0])
+    current_sum = sum(i)
+    if current_sum <= b and current_sum > result:
+        result = current_sum
+print(result)
