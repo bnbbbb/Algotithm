3fac3847fa7ee10ce37c35e5b04f610fcc5401c9
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-12-17 20:26:19 +0900

    [level 2] Title: 숫자의 표현, Time: 5.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = 1
+    for i in range(1, n+1):
+        result = i
+        for j in range(i+1, n+1):
+            result += j 
+            if result == n:
+                answer += 1
+                break
+            elif result > n:
+                break
+    return answer
