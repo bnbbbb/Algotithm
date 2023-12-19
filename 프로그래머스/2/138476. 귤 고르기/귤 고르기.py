fd38ca3fb4bb986aed39a2bf81bf435d0cee0959
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-12-19 17:52:02 +0900

    [level 2] Title: 귤 고르기, Time: 29.26 ms, Memory: 21.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -0,0 +1,11 @@
+from collections import Counter
+def solution(k, tangerine):
+    answer = 0
+    con = Counter(tangerine)
+    con = sorted(con.items(), key=lambda x: x[1], reverse=True)
+    for _, i in con:
+        k -= i
+        answer+=1
+        if k <= 0:
+            break
+    return answer
