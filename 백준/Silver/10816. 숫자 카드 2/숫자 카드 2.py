31a2f8949bb9417ae62c82a365a42ae14af1c9fe
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-12-29 17:21:19 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 584 ms, Memory: 127844 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,11 +1,10 @@
 import sys
 from collections import Counter
-dic = {}
 n = int(sys.stdin.readline())
 a = list(sys.stdin.readline().split())
 c = Counter(a)
 m = int(sys.stdin.readline())
-b = list(sys.stdin.readline().split())
 
-for i in b:
-    print(c[i], end=" ")
+result = [c.get(word, 0) for word in sys.stdin.readline().split()]
+
+print(*result)
