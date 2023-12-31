3d21fc09d8ff19b0088eaf87e4d66462eb79e955
bongsu <94068731+bnbbbb@users.noreply.github.com>
2023-12-31 19:08:15 +0900

    [Silver IV] Title: 가로수, Time: 104 ms, Memory: 38828 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.py
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -1,19 +1,21 @@
 import sys
-input = sys.stdin.readline
-N = int(input())
-trees = [int(input()) for _ in range(N)]
+
+n = int(sys.stdin.readline())
+trees = [int(sys.stdin.readline()) for _ in range(n)]
 
 gaps = [trees[i+1] - trees[i] for i in range(0, len(trees)-1)]
-
 def is_gcd(a, b):
     while b:
         a, b = b, a % b
     return a
-max_gap_root = int(max(gaps) ** (1/2))
+
+
+max_gap = int(max(gaps) ** (1/2))
 
 gcd = gaps[0]
+
 for num in gaps:
     gcd = is_gcd(gcd, num)
 
-result = int((trees[-1] - trees[0])/gcd - N + 1)
+result = int((trees[-1] - trees[0])/gcd - n + 1)
 print(result)
