3f43e688b1b83868e15e2524a453013711263573
bongsu <94068731+bnbbbb@users.noreply.github.com>
2024-01-04 18:47:09 +0900

    [Silver IV] Title: 제로, Time: 3916 ms, Memory: 34016 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,10 @@
+from collections import deque
+arr = deque()
+for _ in range(int(input())):
+    n = int(input())
+    if n == 0:
+        arr.pop()
+    else:
+        arr.append(n)
+
+print(sum(arr))
