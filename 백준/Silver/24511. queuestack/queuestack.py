c798ae4f0433005b6f9a6fa80de69928b6887a28
bongsu <94068731+bnbbbb@users.noreply.github.com>
2024-01-10 20:36:53 +0900

    [Silver III] Title: queuestack, Time: 160 ms, Memory: 47076 KB -BaekjoonHub

--- 백준/Silver/24511. queuestack/queuestack.py
+++ 백준/Silver/24511. queuestack/queuestack.py
@@ -1,19 +1,12 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+m = int(input())
+c = list(map(int, input().split()))
 
-N = int(input())
-sequence_A = list(map(int, input().split())) 
-sequence_B = list(map(int, input().split())) 
-M = int(input())                             
-sequence_C = list(map(int, input().split())) 
-
-queue = deque()
-for i in range(N):
-    if sequence_A[i] == 0:
-        queue.appendleft(sequence_B[i])
-
-for i in range(M):
-    queue.append(sequence_C[i])
-    print(queue.popleft(), end = " ")
+ans = [b[i] for i in range(n) if not a[i]*b[i]][::-1]
+ans += [c[i] for i in range(m - len(ans))]
+print(*ans[:m])
