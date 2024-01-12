2479373ddc49a921768b3ed77eb614822a0411cf
bongsu <94068731+bnbbbb@users.noreply.github.com>
2024-01-12 18:39:36 +0900

    [Silver I] Title: 회의실 배정, Time: 304 ms, Memory: 59068 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,20 @@
+
+import sys
+
+def meeting(meetings):
+    meetings.sort(key=lambda x: (x[1], x[0]))
+    
+    selected_meetings = []
+    end_time = 0
+    
+    for meeting in meetings:
+        if meeting[0] >= end_time:
+            selected_meetings.append(meeting)
+            end_time = meeting[1]
+    return selected_meetings
+
+n = int(input())
+meetings = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
+select_meeting = meeting(meetings)
+print(len(select_meeting))
