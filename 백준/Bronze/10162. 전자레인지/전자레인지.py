92254101252024e8a1607dac5a364ad2dc25ae17
bongsu <94068731+bnbbbb@users.noreply.github.com>
2024-01-16 17:50:41 +0900

    [Bronze III] Title: 전자레인지, Time: 40 ms, Memory: 31120 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.py
@@ -0,0 +1,11 @@
+n = int(input())
+result = []
+arr = [600, 60, 10]
+for i in range(3):
+    if n % 10 != 0:
+        result.append(-1)
+        break
+    else:
+        result.append(n // arr[i])
+        n %= arr[i]
+print(*result)
