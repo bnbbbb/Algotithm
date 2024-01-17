ab8ec1cc7ecf1737cb122712a51a2181232499f0
bongsu <94068731+bnbbbb@users.noreply.github.com>
2024-01-17 18:38:41 +0900

    [Silver V] Title: 뒤집기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,9 +1,24 @@
-n = input();
+def min_flip_count(s):
+    count_0 = 0  # 연속된 0의 그룹 수
+    count_1 = 0  # 연속된 1의 그룹 수
 
-arr1 = n.split('0')
-arr2 = n.split('1')
+    # 문자열의 첫 번째 문자에 대한 초기화
+    if s[0] == '0':
+        count_0 += 1
+    else:
+        count_1 += 1
 
-a = len(arr1) -  arr1.count('')
-b = len(arr2) - arr2.count('')
+    for i in range(1, len(s)):
+        if s[i] != s[i-1]:
+            if s[i] == '0':
+                count_0 += 1
+            else:
+                count_1 += 1
 
-print(min(a , b))
+    return min(count_0, count_1)
+
+# 입력 받기
+S = input()
+
+# 결과 출력
+print(min_flip_count(S))
