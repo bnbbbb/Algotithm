74d7629fd2f1e2bea80a6d0a4e57383b0c066bc9
bongsu <94068731+bnbbbb@users.noreply.github.com>
2024-01-18 17:12:54 +0900

    [Silver I] Title: 신입 사원, Time: 2852 ms, Memory: 59060 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,19 +1,22 @@
 import sys
 input = sys.stdin.readline
+
 t = int(input()) 
-answer =[]
+answer = []
+
 for _ in range(t):
-    n = int(input()) 
-    applicants = [tuple(map(int, input().split())) for _ in range(n)]  
-    applicants.sort(key=lambda x: x[0])
-    min_interview_rank = float('inf')  
-    count = 0  
+    n = int(input())
+    applicants = [tuple(map(int, input().split())) for _ in range(n)]
+    
+    applicants.sort(key=lambda x: x[1])  # 면접 성적을 기준으로 정렬
+    min_interview_rank = float('inf')
+    count = 0
 
     for i in range(n):
-        if applicants[i][1] < min_interview_rank:
-            min_interview_rank = applicants[i][1]
+        if applicants[i][0] < min_interview_rank:
+            min_interview_rank = applicants[i][0]
             count += 1
+            
     answer.append(count)
 
-for i in answer:
-    print(i)
+print('\n'.join(map(str, answer)))
