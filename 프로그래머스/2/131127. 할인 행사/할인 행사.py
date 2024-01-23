83113799ef6d3a1c2e818dabdec33e62d5a6a548
bongsu <94068731+bnbbbb@users.noreply.github.com>
2024-01-23 21:22:21 +0900

    [level 2] Title: 할인 행사, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+def solution(want, number, discount):
+    answer = 0
+    a = dict(zip(want, number))
+    for i in range(len(discount)):
+        result = Counter(discount[i:i+10])
+        if all(result.get(j, 0) >= x for j, x in a.items()):
+            answer += 1       
+    return answer
