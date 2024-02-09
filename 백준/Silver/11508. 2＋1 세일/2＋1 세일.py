4cb8a404840abec4b257cc8a356ac29fcaf7446e
bongsu <94068731+bnbbbb@users.noreply.github.com>
2024-02-09 22:56:13 +0900

    [Silver IV] Title: 2+1 세일, Time: 3940 ms, Memory: 35364 KB -BaekjoonHub

--- 백준/Silver/11508. 2＋1 세일/2＋1 세일.py
+++ 백준/Silver/11508. 2＋1 세일/2＋1 세일.py
@@ -2,7 +2,9 @@ n = int(input())
 milks = [int(input()) for _ in range(n)]
 milks.sort(reverse=True)
 
-discount = sum(milks[2::3])
+discount = 0
+for i in range(2, n, 3):
+    discount += milks[i]
 
 total = sum(milks)
 print(total - discount)
