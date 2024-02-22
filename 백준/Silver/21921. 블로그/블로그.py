31141b537809813f5cf16fc13237cc74059a423a
bongsu <94068731+bnbbbb@users.noreply.github.com>
2024-02-22 23:38:03 +0900

    [Silver III] Title: 블로그, Time: 120 ms, Memory: 58784 KB -BaekjoonHub

---
+++ 백준/Silver/21921. 블로그/블로그.py
@@ -0,0 +1,34 @@
+import sys
+
+def count_visited(n, x, visited):
+    max_visit = 0
+    count_max_visit = 0
+    current_sum = sum(visited[:x])
+
+    if current_sum > max_visit:
+        max_visit = current_sum
+        count_max_visit = 1
+    elif current_sum == max_visit:
+        count_max_visit += 1
+
+    for i in range(x, n):
+        current_sum = current_sum - visited[i - x] + visited[i]
+
+        if current_sum > max_visit:
+            max_visit = current_sum
+            count_max_visit = 1
+        elif current_sum == max_visit:
+            count_max_visit += 1
+            
+    return max_visit, count_max_visit
+
+n, x = map(int, sys.stdin.readline().split())
+visited = list(map(int, sys.stdin.readline().split()))
+
+max_visit, count_max_visit = count_visited(n, x, visited)
+
+if max_visit != 0:
+    print(max_visit)
+    print(count_max_visit)
+else:
+    print('SAD')
