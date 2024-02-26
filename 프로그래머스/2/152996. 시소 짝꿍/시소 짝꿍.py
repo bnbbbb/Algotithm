26d255e791f397b3168546e92f8763c7b2883190
bongsu <94068731+bnbbbb@users.noreply.github.com>
2024-02-26 15:39:31 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+def solution(weights):
+    answer = 0
+    count = Counter(weights)
+    siso = [1/2, 2/3, 3/4]
+    weights = list(set(weights))
+    for k, v in count.items():
+        if v > 1: answer += v * (v-1) / 2
+        
+
+    
+    for w in weights:
+        for s in siso:
+            if w * s in weights:
+                answer += count[w] * count[w*s]
+    return answer      
