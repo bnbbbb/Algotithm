2d6ff260bfbd1753221c5501da4ea7a648f6490f
bongsu <94068731+bnbbbb@users.noreply.github.com>
2024-04-29 18:19:43 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,12 +1,6 @@
-import math
-def solution(n,a,b):
-    result = 1
-    
-    while 1:
-        if (a < b and b-a ==1 and a % 2 ==1) or (a > b and a-b==1 and b%2 == 1):
-            return result
-        else:
-            n = n//2
-            a = math.ceil(a/2)
-            b = math.ceil(b/2)
-            result += 1
+def solution(n, a, b):
+    answer = 0
+    while a != b:
+        a, b = (a//2)+(a%2), (b//2)+(b%2)
+        answer += 1
+    return answer
