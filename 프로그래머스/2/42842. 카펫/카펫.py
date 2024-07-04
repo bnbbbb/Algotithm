1dc7f055cee14bf582b6abbfca2ba882a7f2094a
bongsu <94068731+bnbbbb@users.noreply.github.com>
2024-07-04 19:53:24 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -8,6 +8,7 @@ def solution(brown, yellow):
                 result.append(alc//i)
     high = sorted(result, reverse=True)
     low = sorted(result)
+    
     for x, y in zip(high, low):
         if x >= y and (x-2) * (y-2) == yellow:
             return [x, y]
