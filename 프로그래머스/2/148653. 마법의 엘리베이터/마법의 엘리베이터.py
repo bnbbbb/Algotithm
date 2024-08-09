0777dd0a9c47afe9559cdf5fc2de23b0a7209779
bongsu <94068731+bnbbbb@users.noreply.github.com>
2024-08-09 16:17:36 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -0,0 +1,25 @@
+def solution(storey):
+    answer = 0
+    loop = len(str(storey))
+    div = 10
+    one = storey % div
+    two = (storey-one) % (div * 10)
+    
+    if one == 5 and two >= 50:
+        storey += div - one
+        answer += (div-one) // (div // 10)
+    elif one == 5 and two < 50:
+        storey -= one
+        answer += one // (div // 10)
+ 
+    while storey != 0:
+        mod = storey % div
+        if mod > 5 * div//10:
+            storey += div - mod
+            answer += (div-mod) // (div // 10)
+        else:
+            storey -= mod
+            answer += mod // (div // 10)
+        div *= 10
+        loop -= 1
+    return answer
