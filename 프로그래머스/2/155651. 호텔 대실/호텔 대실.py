7254f9a9643e07c890130bac63d1566b93057ae6
bongsu <94068731+bnbbbb@users.noreply.github.com>
2024-08-10 15:05:56 +0900

    [level 2] Title: 호텔 대실, Time: 24.09 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -0,0 +1,22 @@
+def solution(book_time):
+    book_time.sort()  
+    rooms = []  
+    answer = 0
+    for start, end in book_time:
+        start_h, start_m = map(int, start.split(':'))
+        end_h, end_m = map(int, end.split(':'))
+        
+        start_time = start_h * 60 + start_m
+        end_time = end_h * 60 + end_m
+        
+        available_room = False
+        for i, room in enumerate(rooms):
+            if room <= start_time:
+                rooms[i] = end_time + 10  
+                available_room = True
+                break
+        if not available_room:
+            rooms.append(end_time + 10)  
+            answer += 1  
+    
+    return answer
