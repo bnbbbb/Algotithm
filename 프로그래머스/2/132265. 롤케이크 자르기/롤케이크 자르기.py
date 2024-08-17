0cd1112eb415eb81dc5ef4a2e0b38964e12f877f
bongsu <94068731+bnbbbb@users.noreply.github.com>
2024-08-17 22:08:42 +0900

    [level 2] Title: 롤케이크 자르기, Time: 623.38 ms, Memory: 50.5 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,15 +1,35 @@
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
 from collections import Counter
 
 def solution(topping):
     answer = 0
-    bro = Counter(topping)
-    smbro = set()
+    dic = Counter(topping)
+    set_dic = set()
+    answer = 0
+
     for i in topping:
-        bro[i] -= 1
-        if bro[i] == 0:
-            del bro[i]
-        smbro.add(i)
-        if len(bro) == len(smbro):
+        dic[i] -= 1
+        set_dic.add(i)
+        if dic[i] == 0:
+            dic.pop(i)
+        if len(dic) == len(set_dic):
             answer += 1
-            
+
     return answer
