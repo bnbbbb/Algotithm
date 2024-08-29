792cf225a35642ec3a2ae3aa3d452e6fc253097a
bongsu <94068731+bnbbbb@users.noreply.github.com>
2024-08-29 11:42:42 +0900

    [level 2] Title: 광물 캐기, Time: 109.12 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -0,0 +1,33 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+def solution(picks, minerals):
+    def solve(picks, minerals, fatigue):
+        if sum(picks) == 0 or len(minerals) == 0:
+            return fatigue
+        result = [float('inf')]
+        for i, fatigues in enumerate(({"diamond": 1, "iron": 1, "stone": 1},
+                                      {"diamond": 5, "iron": 1, "stone": 1},
+                                      {"diamond": 25, "iron": 5, "stone": 1},)):
+            if picks[i] > 0:
+                temp_picks = picks.copy()
+                temp_picks[i] -= 1
+                result.append(
+                    solve(temp_picks, minerals[5:], fatigue + sum(fatigues[mineral] for mineral in minerals[:5])))
+        return min(result)
+
+    return solve(picks, minerals, 0)
